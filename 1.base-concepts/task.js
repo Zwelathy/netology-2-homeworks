44a28d5057328005950fa6db76b9d05fdc695542
Zwelathy <tessen666@yandex.ru>
2022-06-29 17:52:35 +0300

    feat: first task without second

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let result = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return result;
+  } else if (d == 0) {
+    result = (-b) / (2 * a);
+  } else {
+    result.push((-b + Math.sqrt(d)) / (2 * a));
+    result.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  
+  return result;
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
