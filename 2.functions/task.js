ad8e29e18581ef10de324530adedb66e976e5277
Zwelathy <tessen666@yandex.ru>
2022-07-06 19:12:55 +0300

    fix: first task resolve

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -6,13 +6,17 @@ function getArrayParams(arr) {
   let avg = 0;
 
   for (i = 0; i < arr.length; i++) {
-    if(arr[i] < max) {
+    if(arr[i] > max) {
+      max = arr[i];
+      sum += arr[i];
+      avg = sum / arr.length;
+    }
+    else if (arr[i] < min) {
       min = arr[i];
       sum += arr[i];
       avg = sum / arr.length;
     }
     else {
-      max = arr[i];
       sum += arr[i];
       avg = sum / arr.length;
     }
