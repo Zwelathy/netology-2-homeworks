5b23fdc13967907aea81ab2a08570088c9497df2
Zwelathy <tessen666@yandex.ru>
2022-07-08 14:42:20 +0300

    feat: add tasks resolve

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
+  let a = arr1;
+  let b = arr2;
 
-  // Ваш код
+  if (a.length < b.length) {
+    a = arr2;
+    b = arr1;
+  }
 
-  return result; // boolean
+  result = a.every((key, inx) => key === b[inx]);
+
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(key => key > 0).filter(key => key % 3 === 0).map(key => key * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
