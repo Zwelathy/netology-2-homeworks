31450aeddf11ea49061e7b818a030e7492df970d
Zwelathy <tessen666@yandex.ru>
2022-07-11 21:50:58 +0300

    feat: add task resolve

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let pasha = new Student('Pasha', 'female', 5);
+let sveta = new Student('Sveta', 'diplodok', 14);
+let uasya = new Student('UasyaChotkey', 'muzhik', 39);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = mark;
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let result = this.marks.reduce((a, b) => a + b, 0) / this.marks.length;
+
+  return result;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+
+  this.excluded = reason;
+}
