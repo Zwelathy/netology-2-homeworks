f5172b8fc51105209b238f89ae9d996c64b168a5
Zwelathy <tessen666@yandex.ru>
2022-07-27 21:01:29 +0300

    feat: add resolved tasks

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,60 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+
+    let index = cache.findIndex((key) => key.hash == hash);
+    if (index !== -1) {
+      console.log("Из кэша: " + cache[index].value);
+
+      return "Из кэша: " + cache[index].value;
+    }
+
+    let result = func(...args);
+    cache.push({
+      'hash': hash,
+      'value': result
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer = null;
+
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+  }
+
+  return wrapper;
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timer = null;
+
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    wrapper.count++;
+  }
+  wrapper.count = 0;
+  
+  return wrapper;
 }
